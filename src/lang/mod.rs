44a6ac9a42488d17dbfb561c173fc1daa93999c4
DavidLee18 <djwodus@gmail.com>
2024-12-22 08:25:12 +0900

    a basic `Display` impl

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -17,6 +17,7 @@ enum Expr<A> {
 }
 
 type Name = String;
+type CoreExpr = Expr<Name>;
 type Alter<A> = (i64, Vec<A>, Expr<A>);
 type CoreAlt = Alter<Name>;
 
@@ -53,8 +54,70 @@ const SIMPLE_PROGRAM: CoreProgram = vec![
     ),
 ];
 
-const PRELUDE_DEFS: CoreProgram = vec![(
-    Name::from("I"),
-    vec![Name::from("x")],
-    Expr::Var(Name::from("x")),
-)];
+const PRELUDE_DEFS: CoreProgram = vec![
+    (
+        Name::from("I"),
+        vec![Name::from("x")],
+        Expr::Var(Name::from("x")),
+    ),
+    (
+        Name::from("K"),
+        vec![Name::from("x"), Name::from("y")],
+        Expr::Var(Name::from("x")),
+    ),
+    (
+        Name::from("K1"),
+        vec![Name::from("x"), Name::from("y")],
+        Expr::Var(Name::from("y")),
+    ),
+    (
+        Name::from("S"),
+        vec![Name::from("f"), Name::from("g"), Name::from("x")],
+        Expr::Ap(
+            Box::new(Expr::Ap(
+                Box::new(Expr::Var(Name::from("f"))),
+                Box::new(Expr::Var(Name::from("x"))),
+            )),
+            Box::new(Expr::Ap(
+                Box::new(Expr::Var(Name::from("g"))),
+                Box::new(Expr::Var(Name::from("x"))),
+            )),
+        ),
+    ),
+    (
+        Name::from("compose"),
+        vec![Name::from("f"), Name::from("g"), Name::from("x")],
+        Expr::Ap(
+            Box::new(Expr::Var(Name::from("f"))),
+            Box::new(Expr::Ap(
+                Box::new(Expr::Var(Name::from("g"))),
+                Box::new(Expr::Var(Name::from("x"))),
+            )),
+        ),
+    ),
+    (
+        Name::from("twice"),
+        vec![Name::from("f")],
+        Expr::Ap(
+            Box::new(Expr::Ap(
+                Box::new(Expr::Var(Name::from("compose"))),
+                Box::new(Expr::Var(Name::from("f"))),
+            )),
+            Box::new(Expr::Var(Name::from("f"))),
+        ),
+    ),
+];
+
+impl std::fmt::Display for CoreExpr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CoreExpr::Var(v) => write!(f, "{}", v),
+            CoreExpr::Num(n) => write!(f, "{}", n),
+            CoreExpr::Constr { .. } => todo!(),
+            CoreExpr::Ap(e1, e2) => write!(f, "{} {}", e1, e2),
+            CoreExpr::Let { .. } => todo!(),
+            CoreExpr::Case(_, _) => todo!(),
+            CoreExpr::Lam(_, _) => todo!(),
+        }
+    }
+}
