55d4da800c005cf32925c8fbfd38ca6fab7a4576
DavidLee18 <djwodus@gmail.com>
2025-01-11 14:38:30 +0900

    Exercise 1.11. (p. 31)

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use tailcall::tailcall;
+
+#[derive(Debug, Clone)]
 enum Expr<A> {
     Var(Name),
     Num(i64),
@@ -32,81 +34,85 @@ type CoreScDefn = ScDefn<Name>;
 /// main = double 21 ;
 /// double x = x + x
 /// ```
-const SIMPLE_PROGRAM: CoreProgram = vec![
-    (
-        Name::from("main"),
-        vec![],
-        Expr::Ap(
-            Box::new(Expr::Var(Name::from("double"))),
-            Box::new(Expr::Num(21)),
+fn simple_program() -> CoreProgram {
+    vec![
+        (
+            Name::from("main"),
+            vec![],
+            Expr::Ap(
+                Box::new(Expr::Var(Name::from("double"))),
+                Box::new(Expr::Num(21)),
+            ),
         ),
-    ),
-    (
-        Name::from("double"),
-        vec![Name::from("x")],
-        Expr::Ap(
-            Box::new(Expr::Ap(
-                Box::new(Expr::Var(Name::from("+"))),
+        (
+            Name::from("double"),
+            vec![Name::from("x")],
+            Expr::Ap(
+                Box::new(Expr::Ap(
+                    Box::new(Expr::Var(Name::from("+"))),
+                    Box::new(Expr::Var(Name::from("x"))),
+                )),
                 Box::new(Expr::Var(Name::from("x"))),
-            )),
-            Box::new(Expr::Var(Name::from("x"))),
+            ),
         ),
-    ),
-];
+    ]
+}
 
-const PRELUDE_DEFS: CoreProgram = vec![
-    (
-        Name::from("I"),
-        vec![Name::from("x")],
-        Expr::Var(Name::from("x")),
-    ),
-    (
-        Name::from("K"),
-        vec![Name::from("x"), Name::from("y")],
-        Expr::Var(Name::from("x")),
-    ),
-    (
-        Name::from("K1"),
-        vec![Name::from("x"), Name::from("y")],
-        Expr::Var(Name::from("y")),
-    ),
-    (
-        Name::from("S"),
-        vec![Name::from("f"), Name::from("g"), Name::from("x")],
-        Expr::Ap(
-            Box::new(Expr::Ap(
+fn prelude_defs() -> CoreProgram {
+    vec![
+        (
+            Name::from("I"),
+            vec![Name::from("x")],
+            Expr::Var(Name::from("x")),
+        ),
+        (
+            Name::from("K"),
+            vec![Name::from("x"), Name::from("y")],
+            Expr::Var(Name::from("x")),
+        ),
+        (
+            Name::from("K1"),
+            vec![Name::from("x"), Name::from("y")],
+            Expr::Var(Name::from("y")),
+        ),
+        (
+            Name::from("S"),
+            vec![Name::from("f"), Name::from("g"), Name::from("x")],
+            Expr::Ap(
+                Box::new(Expr::Ap(
+                    Box::new(Expr::Var(Name::from("f"))),
+                    Box::new(Expr::Var(Name::from("x"))),
+                )),
+                Box::new(Expr::Ap(
+                    Box::new(Expr::Var(Name::from("g"))),
+                    Box::new(Expr::Var(Name::from("x"))),
+                )),
+            ),
+        ),
+        (
+            Name::from("compose"),
+            vec![Name::from("f"), Name::from("g"), Name::from("x")],
+            Expr::Ap(
                 Box::new(Expr::Var(Name::from("f"))),
-                Box::new(Expr::Var(Name::from("x"))),
-            )),
-            Box::new(Expr::Ap(
-                Box::new(Expr::Var(Name::from("g"))),
-                Box::new(Expr::Var(Name::from("x"))),
-            )),
+                Box::new(Expr::Ap(
+                    Box::new(Expr::Var(Name::from("g"))),
+                    Box::new(Expr::Var(Name::from("x"))),
+                )),
+            ),
         ),
-    ),
-    (
-        Name::from("compose"),
-        vec![Name::from("f"), Name::from("g"), Name::from("x")],
-        Expr::Ap(
-            Box::new(Expr::Var(Name::from("f"))),
-            Box::new(Expr::Ap(
-                Box::new(Expr::Var(Name::from("g"))),
-                Box::new(Expr::Var(Name::from("x"))),
-            )),
-        ),
-    ),
-    (
-        Name::from("twice"),
-        vec![Name::from("f")],
-        Expr::Ap(
-            Box::new(Expr::Ap(
-                Box::new(Expr::Var(Name::from("compose"))),
+        (
+            Name::from("twice"),
+            vec![Name::from("f")],
+            Expr::Ap(
+                Box::new(Expr::Ap(
+                    Box::new(Expr::Var(Name::from("compose"))),
+                    Box::new(Expr::Var(Name::from("f"))),
+                )),
                 Box::new(Expr::Var(Name::from("f"))),
-            )),
-            Box::new(Expr::Var(Name::from("f"))),
+            ),
         ),
-    ),
-];
+    ]
+}
 
 impl std::fmt::Display for CoreExpr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -120,4 +126,72 @@ impl std::fmt::Display for CoreExpr {
             CoreExpr::Lam(_, _) => todo!(),
         }
     }
+}
+
+type Token = (u32, String);
+
+fn clex(input: String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut chars = input.chars();
+    let mut c = chars.next();
+    let mut line = 0;
+    let mut temp;
+    while let Some(c_) = c {
+        match c_ {
+            '|' => {
+                let c_next = chars.next();
+                match c_next {
+                    Some('|') => {
+                        while chars.next() != Some('\n') {}
+                        line += 1;
+                    },
+                    _ => tokens.push((line, String::from('|'))),
+                }
+            }
+            c__ if TWO_CHAR_OPS.iter().find(|&&s| s.starts_with(c__)).is_some() => {
+                match TWO_CHAR_OPS.iter().find(|&&s| s.starts_with(c__)) {
+                    Some(&"==") => tokens.push((line, String::from("=="))),
+                    Some(&"~=") => tokens.push((line, String::from("~="))),
+                    Some(&">=") => tokens.push((line, String::from(">="))),
+                    Some(&"<=") => tokens.push((line, String::from("<="))),
+                    Some(&"->") => tokens.push((line, String::from("->"))),
+                    _ => unreachable!(),
+                }
+            }
+            c__ if c__.is_whitespace() => {
+                if c__ == '\n' { line += 1; }
+            }
+            c__ if c__.is_digit(10) => {
+                let mut rest = chars
+                    .clone()
+                    .take_while(|c| c.is_digit(10))
+                    .collect::<String>();
+                rest.insert(0, c__);
+                tokens.push((line, rest));
+                temp = chars.skip_while(|c| c.is_digit(10)).collect::<String>();
+                chars = temp.chars();
+            }
+            c__ if c__.is_alphabetic() => {
+                let mut rest = chars
+                    .clone()
+                    .take_while(|c| c.is_alphanumeric() || *c == '_')
+                    .collect::<String>();
+                rest.insert(0, c__);
+                tokens.push((line, rest));
+                temp = chars
+                    .skip_while(|c| c.is_alphanumeric() || *c == '_')
+                    .collect::<String>();
+                chars = temp.chars();
+            }
+            c__ => tokens.push((line, String::from(c__))),
+        }
+        c = chars.next();
+    }
+    tokens
+}
+
+const TWO_CHAR_OPS: [&'static str; 5] = ["==", "~=", ">=", "<=", "->"];
+
+fn syntax(tokens: Vec<Token>) -> CoreProgram {
+    todo!()
 }
